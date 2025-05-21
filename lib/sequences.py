b5ba08f292f74d8eed0b1aa79d4f58338ebc9962
Brian <brian2000bett@gmail.com>
2025-05-21 08:20:29 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence=[0,1]
+    for _ in range(2,length):
+        sequence.append(sequence[-1]+sequence[-2])
+    print(sequence[:length])
